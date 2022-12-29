1224e724276c91567380d8b872e7fbd8eb75a1a0
KoraySerhan <serhankoray@hotmail.com>
2022-12-29 04:12:12 +0300

    body button

--- MyApplication/app/src/main/java/com/example/myapplication/Menu.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Menu.java
@@ -5,18 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
+    private Button button;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        button =(Button) findViewById(R.id.bodyInfoButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openbody();
+            }
+        });
 
     }
     public void counterScreen(View view){
         Intent intent = new Intent(this, Counter.class);
+        startActivity(intent);
+    }
+    public void openbody(){
+        Intent intent = new Intent(this,body.class);
         startActivity(intent);
     }
 }

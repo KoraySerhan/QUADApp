4936410349dd734c7141247e337115d5b0dcb6f1
KoraySerhan <serhankoray@hotmail.com>
2023-01-15 01:49:37 +0300

    log in

---
+++ MyApplication/app/src/main/java/com/example/myapplication/KayitOlmaSayfasi.java
@@ -0,0 +1,54 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class KayitOlmaSayfasi extends AppCompatActivity {
+    private Button button;
+    private Button button1;
+    EditText Name,  Psd;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kayit_olma_sayfasi);
+
+        button1 = (Button) findViewById(R.id.btnGirişeDön);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGiris();
+            }
+        });
+
+        preferences = getSharedPreferences("MyPreferences",MODE_PRIVATE);
+        editor = preferences.edit();
+
+        Name = findViewById(R.id.kayıtKullanıcıAdı);
+        Psd = findViewById(R.id.kayıtParola);
+        button = (Button) findViewById(R.id.btnKaydet);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String my_name =  Name.getText().toString();
+                String my_password = Psd.getText().toString();
+                editor.putString("Saved_Name", my_name);
+                editor.putString("Saved_password",my_password);
+                editor.commit();
+                openGiris();
+            }
+        });
+    }
+    public void openGiris(){
+        Intent intent = new Intent(this, Giris.class);
+        startActivity(intent);
+    }
+}
